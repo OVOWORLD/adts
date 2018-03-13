ab0fd8842b33c08d0eba29206fba4be69219bbbf
OVOWORLD <OVOWORLD@users.noreply.github.com>
2018-03-13 11:01:25 -0400

    StackV.cpp

--- ListClient.cpp
+++ ListClient.cpp
@@ -12,6 +12,17 @@ int main()
  // ...
  L1.insert(5,1);
  L1.insert(4,2);
+ L1.insert(3,1);
+ L1.insert(2,3);
+ L1.insert(7,4);
+ L1.insert(10,5);
  L2.insert(3,1);
-
+ L2.insert(5,2);
+ L2.insert(7,3);
+ L2.insert(6,4);
+ 
+ for(int i=1;i<=L1.size();i++)
+ {
+ cout << L1.get(i) << endl;
+}
 }
--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,32 @@
 #include "StackV.h"
+#include <iostream>
+using namespace std;
+
+int Stack::size()
+{
+	return data.size();
+}
+
+void Stack::push(int d)
+{
+	data.push_back(d);
+}
+
+void Stack::pop()
+{
+	data.pop_back();
+}
+
+int Stack::top()
+{
+	return data[data.size()-1];
+}
+
+void Stack::clear()
+{
+	for(unsigned int i=0;i<=data.size();i++)
+	{
+		data.pop_back();
+	}	
+	
+}
