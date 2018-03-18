99ea03e95fbb0a2b941165d92d18ca4fdc2af28e
OVOWORLD <OVOWORLD@users.noreply.github.com>
2018-03-17 23:43:12 -0400

    StackL.cpp

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,38 @@
 #include "StackL.h"
+#include "list.h"
+#include <iostream>
+using namespace std;
+
+class Stack::list
+{
+	public:
+	
+	int num = 0;
+	
+};
+
+
+int Stack::size()
+{
+	return data;
+}
+
+void Stack::push(int d)
+{
+	
+}
+
+void Stack::pop()
+{
+	
+}
+
+int Stack::top()
+{
+	
+}
+
+void Stack::clear()
+{
+	
+}
