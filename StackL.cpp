b417e691127ca92ee3afa157923fac9671ac4ed9
OVOWORLD <OVOWORLD@users.noreply.github.com>
2018-03-20 11:12:19 -0400

    StackL.cpp

--- StackL.cpp
+++ StackL.cpp
@@ -1,38 +1,33 @@
 #include "StackL.h"
-#include "list.h"
+#include "List.h"
 #include <iostream>
 using namespace std;
-
-class Stack::list
-{
-	public:
-	
-	int num = 0;
-	
-};
 
 
 int Stack::size()
 {
-	return data;
+	return data.size();
 }
 
 void Stack::push(int d)
 {
-	
+	data.insert(d, 1);
 }
 
 void Stack::pop()
 {
-	
+	data.remove(1);
 }
 
 int Stack::top()
 {
-	
+  return data.get(1);
+  
 }
 
 void Stack::clear()
 {
-	
+	// you can also use data.clear();
+	while(data.size() > 0)
+		data.remove(1); // or you can use pop(); instead
 }
